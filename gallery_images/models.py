d46290aa043b03b347acdb87c98573266afd1577
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-25 02:28:50 +0300

    Created the first model. The images model.

--- gallery_images/models.py
+++ gallery_images/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length = 30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.image_name
+    
