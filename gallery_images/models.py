ec8cbe69859bd17bf9621b085a85de51804abd14
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-25 02:32:51 +0300

    Created the Location model.

--- gallery_images/models.py
+++ gallery_images/models.py
@@ -14,3 +14,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
