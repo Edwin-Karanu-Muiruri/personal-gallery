9ac0f66b7450ddaf3462c99b62ad30d9ef09e9dd
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-25 23:41:47 +0300

    Registered my models to the admin site to be able to upload images.

--- Django Projects/django-gallery/imageapp/admin.py
+++ Django Projects/django-gallery/imageapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category,Location,Image
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image)
