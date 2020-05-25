c50a2331da600dbdf8849702cb2a375725b4b126
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-25 05:03:46 +0300

    Created the view function for the created index path.

--- gallery_images/views.py
+++ gallery_images/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Image, Category, Location
 
 # Create your views here.
+
+def index(request):
+    '''
+    View function to render index page
+    '''
+    title = " Personal Gallery"
+    images = Image.display_all_images()
+    return render(request, "index.html", {"title": title, "images":images})
