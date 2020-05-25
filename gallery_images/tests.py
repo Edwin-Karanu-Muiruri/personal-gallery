31ef25167c732c4941fd7e691dbd76de572ada65
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-25 08:18:41 +0300

    Added the search and location view functions.

--- gallery_images/tests.py
+++ gallery_images/tests.py
@@ -36,9 +36,9 @@ class CategoryTestClass(TestCase):
         Tests whether the category name is updated
         '''
         self.nature.save_category()
-        self.nature.update_category(self.nature.id,'natural')
-        new_update = Category.objects.get(name = "natural")
-        self.assertEqual(new_update.name, 'natural')
+        self.nature.update_category(self.nature.id,'wild nature')
+        new_update = Category.objects.get(name = "wild nature")
+        self.assertEqual(new_update.name, 'wild nature')
 
 class ImageTestClass(TestCase):
     '''
@@ -46,9 +46,9 @@ class ImageTestClass(TestCase):
     '''
 
     def setUp(self):
-        self.nature = Category( name= "nature")
+        self.nature = Category( name= "Family")
         self.nairobi = Location(name = "nairobi")
-        self.flower = Image(image = "image", name ='flower', description = 'beautiful', category= self.nature, location= self.nairobi)
+        self.flower = Image(image = "image", name ='flower', description = 'beautiful', category= self.Family, location= self.nairobi)
 
         self.nature.save_category()
         self.nairobi.save_location()
@@ -82,9 +82,9 @@ class ImageTestClass(TestCase):
 
     def test_update_image_description(self):
         self.flower.save_image()
-        self.flower.update_image_description(self.flower.id,'pink')
+        self.flower.update_image_description(self.flower.id,'rose')
         new_update = Image.objects.get(name = "flower")
-        self.assertEqual(new_update.description, 'pink')
+        self.assertEqual(new_update.description, 'rose')
 
 
     def test_get_image_by_id(self):
@@ -95,7 +95,7 @@ class ImageTestClass(TestCase):
     def test_search_image(self):
         self.nature.save_category()
         self.flower.save_image()
-        images = Image.search_image("nature")
+        images = Image.search_image("Family")
         self.assertTrue(len(images) > 0)
 
     
@@ -104,3 +104,40 @@ class ImageTestClass(TestCase):
         self.flower.save_image()
         images = Image.filter_by_location("nairobi")
         self.assertTrue(len(images) > 0)
+
+class LocationTestClass(TestCase):
+    '''
+    Location class test
+    '''
+    def setUp(self):
+        self.nairobi = Location(name = "nairobi")
+        self.nairobi.save_location()
+    
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_location_instance(self):
+        self.assertTrue(isinstance(self.nairobi, Location))
+
+    def test_save_location_method(self):
+        self.nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)> 0)
+
+    def test_delete_location(self):
+        self.nairobi.save_location()
+        locations1= Location.objects.all()
+        self.assertEqual(len(locations1),1)
+        self.nairobi.delete_location()
+        locations2= Location.objects.all()
+        self.assertEqual(len(locations2),0)
+
+    def test_update_location(self):
+        self.nairobi.save_location()
+        self.nairobi.update_location(self.nairobi.id,'Kiambu')
+        new_update = Location.objects.get(name = "Kiambu")
+        self.assertEqual(new_update.name, 'Kiambu')
+
+    def test_display_locations(self):
+        self.nairobi.save_location()
+        self.assertEqual(len(Location.display_all_locations()), 1)
--- gallery_images/views.py
+++ gallery_images/views.py
@@ -10,3 +10,28 @@ def index(request):
     title = " Personal Gallery"
     images = Image.display_all_images()
     return render(request, "index.html", {"title": title, "images":images})
+def image_search(request):
+    '''
+    Displays the search results page
+    '''
+
+    title = "Search results"
+    if "category" in request.GET and request.GET["category"]:
+        searched_category = request.GET.get("category")
+        searched_images = Image.search_image(searched_category)
+        message = f"{searched_category}"
+        
+        return render(request, 'search.html', {"message":message, "searched_images": searched_images,"title": title})
+
+
+    else:
+        message = "You haven't searched for anything"
+        return render(request, 'search.html', {"message":message,"title": title})
+
+def location_images(request,location):
+    '''
+    Function to display images per location 
+    '''
+    location_images = Image.filter_by_location(location)
+
+    return render(request, 'location.html', { "location_images" :location_images, "location":location})
