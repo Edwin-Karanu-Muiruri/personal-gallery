3421f4038c58920ea5892bcdce89fc56673bc2d0
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-25 15:42:59 +0300

    Created the index view function and html page

--- Django Projects/django-gallery/imageapp/views.py
+++ Django Projects/django-gallery/imageapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    '''
+    View function to render the index page
+    '''
+    return render(request,"index.html")
