058b15de3d3846c59d0af88ce1c481314a6cc995
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-26 00:40:11 +0300

    Defined the view function and its url and added the link to the homepage on the navbar.

--- Django Projects/django-gallery/imageapp/urls.py
+++ Django Projects/django-gallery/imageapp/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index,name = 'welcome')
+    path('',views.index,name = "index")
 ]
 
 if settings.DEBUG:
--- Django Projects/django-gallery/imageapp/views.py
+++ Django Projects/django-gallery/imageapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Category,Location,Image
 
 # Create your views here.
 def index(request):
     '''
     View function to render the index page
     '''
-    return render(request,"index.html")
+    title = "My Gallery"
+    images = Image.display_all_images()
+    locations = Location.display_all_locations()
+    return render(request,"index.html", {"title":title, "images":images, "locations":locations})
