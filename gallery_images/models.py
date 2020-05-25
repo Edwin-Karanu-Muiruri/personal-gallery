902d452392a3a50c22ff3b53b3ddcf741274dad0
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-25 04:58:51 +0300

    Defined the media root and urls.

--- gallery_images/models.py
+++ gallery_images/models.py
@@ -1,22 +1,36 @@
 from django.db import models
 
 # Create your models here.
-class Images(models.Model):
-    image = models.ImageField(upload_to = 'images/')
-    image_name = models.CharField(max_length = 30)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.image_name
     
 class Category(models.Model):
-    name = models.CharField(max_length = 30)
+    CATEGORY_CHOICES = [
+        ('Recent','Recent'),
+        ('Family','Family'),
+        ('Friends','Friends'),
+        ('Travel','Travel'),
+        ('Food','Food'),
+        ('Fashion','Fashion'),
+        ('Sports','Sports'),
+    ]
+    category = models.CharField(max_length = 30,choices = CATEGORY_CHOICES, default = 'Recent')
+
 
     def __str__(self):
-        return self.name
+        return self.category
 
 class Location(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.name
+
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length = 30)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete = models.CASCADE,default = 'id')
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
+    
--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+
             ],
         },
     },
@@ -121,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
