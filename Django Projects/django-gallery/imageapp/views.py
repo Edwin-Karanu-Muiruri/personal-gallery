ecf280b5875bf1e4872452fe405005e69e67005c
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-26 04:11:27 +0300

    defined the view function to search for image categories.

--- Django Projects/django-gallery/imageapp/views.py
+++ Django Projects/django-gallery/imageapp/views.py
@@ -10,3 +10,20 @@ def index(request):
     images = Image.display_all_images()
     locations = Location.display_all_locations()
     return render(request,"index.html", {"title":title, "images":images, "locations":locations})
+
+def image_search(request):
+    '''
+    View function to display results of the search
+    '''
+    title = "Search Results"
+    if "category" in request.GET and request.GET["category"]:
+        searched_category = request.GET.get("category")
+        searched_images = Image.search_image(searched_category)
+        message = f"{searched_category}"
+
+        return render(request, 'search.html', {"message":message, "searched_images": searched_images,"title": title})
+
+
+    else:
+        message = "Please input a category to be searched"
+        return render(request, 'search.html', {"message":message,"title": title})
